8f90a3e0004e6d199a614c2069145438a1926884
dinoflower <kaedilove@gmail.com>
2024-01-18 15:22:42 -0600

    add placeholder methods

--- hashmap.rb
+++ hashmap.rb
@@ -1,4 +1,9 @@
 class HashMap
+  def initialize
+    @buckets = 16
+    @load_factor = 0.75
+  end
+
   def hash(value)
     hash_code = 0
     prime_number = 17
@@ -7,4 +12,22 @@ class HashMap
 
     hash_code
   end
+
+  def set(key, value); end
+
+  def get(key); end
+
+  def key?(key); end
+
+  def remove(key); end
+
+  def length; end
+
+  def clear; end
+
+  def keys; end
+
+  def values; end
+
+  def entries; end
 end
